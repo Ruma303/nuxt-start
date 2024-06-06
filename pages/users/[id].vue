4486067bb3c5ac0d1dd9dd7d527cd4c91acd83ff
Ruma303 <matteoh8@hotmail.it>
2024-06-06 22:26:30 +0200

    wildcards

---
+++ pages/users/[id].vue
@@ -0,0 +1,14 @@
+<script setup>
+    const route = useRouter();
+    console.log(route.params.id);
+</script>
+
+<template>
+    <div class="mx-auto">
+        <h1 class="p-2 text-4xl font-semibold h-fit">
+            Wildcard: {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
