7c3865074c62b240831aa6e9e09f881234e2285c
Ruma303 <matteoh8@hotmail.it>
2024-06-07 10:52:56 +0200

    logo

---
+++ components/Header/Logo/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import logo_image from "@@/img/logo.png";
+</script>
+
+<template>
+    <img
+        :src="logo_image"
+        alt="Nuxt Logo"
+        class="mx-auto h-12 place-self-start object-contain object-center"
+    />
+</template>
--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,16 +1,8 @@
-<script setup>
-import logo_image from "@@/img/logo.png";
-</script>
-
 <template>
     <header
         class="w-full grid grid-cols-3 items-center shadow-md shadow-gray-200 rounded-t-lg p-2"
     >
-        <img
-            :src="logo_image"
-            alt="Nuxt Logo"
-            class="mx-auto h-12 place-self-start object-contain object-center"
-        />
+        <HeaderLogo />
         <HeaderNavbar />
     </header>
 </template>
