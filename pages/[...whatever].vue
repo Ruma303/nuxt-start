f1f9778390c9b63653047e20c562769a9698eee6
Ruma303 <matteoh8@hotmail.it>
2024-06-07 14:40:37 +0200

    fallback

---
+++ pages/[...whatever].vue
@@ -0,0 +1,17 @@
+<script setup>
+const route = useRoute();
+const whatEver = ref(route.params.whatever);
+
+onMounted(() => {
+    whatEver.value = route.params.whatever;
+});
+</script>
+
+<template>
+    <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
+        <h3 class="mt-2 p-2 text-2xl h-fit text-center">
+            The path <span class="font-bold">{{ whatEver }}</span> you searched
+            for is not found
+        </h3>
+    </div>
+</template>
---
+++ pages/users/role-[type]/[name].vue
@@ -0,0 +1,23 @@
+<script setup>
+const route = useRoute();
+const userType = ref(route.params.type);
+const userName = ref(route.params.name);
+
+onMounted(() => {
+    userType.value = route.params.type;
+    userName.value = route.params.name;
+});
+</script>
+
+<template>
+    <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
+        <h1 class="mt-2 p-2 text-4xl font-semibold h-fit text-center">
+            Role: {{ userType }}
+        </h1>
+        <h3 class="mt-2 p-2 text-3xl font-semibold h-fit text-center">
+            User ID: {{ userName }}
+        </h3>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
