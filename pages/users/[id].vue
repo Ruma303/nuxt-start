041e6695a614ea081f2f5cceae92f985d4fc09f8
Ruma303 <matteoh8@hotmail.it>
2024-06-07 15:21:40 +0200

    inline middleware

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -13,12 +13,22 @@ const id = ref(null);
 onMounted(() => {
     id.value = route.params.id;
 });
+
+definePageMeta({
+    middleware: [
+        function (to, from) {
+            console.log("middleware before page load");
+            console.log("From: ", from);
+            console.log("To: ", to);
+        },
+    ],
+});
 </script>
 
 <template>
     <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
         <h1 class="mt-2 p-2 text-4xl font-semibold h-fit text-center">
-            Wildcard: {{ id }}
+            Wildcard: /{{ id }}
         </h1>
     </div>
 </template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+const userID = ref(null);
+</script>
 
 <template>
     <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
@@ -7,6 +9,23 @@
         >
             All Users
         </h1>
+        <div class="flex flex-col items-center gap-4 w-full mx-auto rounded-lg">
+            <input
+                v-model="userID"
+                type="number"
+                class="p-2 w-1/2 mx-auto rounded-lg border-2 border-blue-600 focus:outline-none focus:border-blue-400"
+                placeholder="Search user by id..."
+            />
+            <p>
+                <NuxtLink
+                    v-if="userID"
+                    :to="`/users/${userID}`"
+                    class="p-2 w-1/2 mx-auto rounded-lg border-2 text-center text-blue-600 hover:bg-blue-600 hover:text-white focus:outline-none focus:border-blue-400"
+                >
+                    Go to user {{ userID }}
+                </NuxtLink>
+            </p>
+        </div>
     </div>
 </template>
 
