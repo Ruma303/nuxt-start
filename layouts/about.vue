357b1939d8019c001c961f2d7d6167a59285b69f
Ruma303 <matteoh8@hotmail.it>
2024-06-07 13:54:24 +0200

    .

---
+++ layouts/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="CustomLayout" class="bg-amber-400 flex flex-col gap-2">
+        <p class="text-md p-4 text-slate-800 font-semibold">About Layout</p>
+        <slot />
+    </div>
+</template>
+ı
