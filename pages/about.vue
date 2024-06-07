28b12f411bde8d6a913b7c02ccc4c4ea5568870b
Ruma303 <matteoh8@hotmail.it>
2024-06-07 12:08:06 +0200

    layouts

--- pages/about.vue
+++ pages/about.vue
@@ -1,17 +1,17 @@
-<script setup lang="ts">
-definePageMeta({
-    layout: "about",
-});
-</script>
+    <script setup lang="ts">
+        definePageMeta({
+            layout: "about",
+        });
+    </script>
 
-<template>
-    <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
-        <h1
-            class="mt-2 p-2 text-4xl font-semibold h-fit text-violet-600 text-center"
-        >
-            About Page
-        </h1>
-    </div>
-</template>
+    <template>
+        <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
+            <h1
+                class="mt-2 p-2 text-4xl font-semibold h-fit text-violet-600 text-center"
+            >
+                About Page
+            </h1>
+        </div>
+    </template>
 
 <style scoped lang="scss"></style>
