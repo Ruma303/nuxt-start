4d4d49f63dc1c49de10038621c4b6695122d6b3b
Ruma303 <matteoh8@hotmail.it>
2024-06-07 14:10:22 +0200

    wildcard ok

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -1,16 +1,26 @@
+<!-- <script setup>
+const route = useRouter();
+const id = route.params.id;
+// console.log(route.params.id);
+const nuxt = useNuxtApp();
+// console.log(nuxt);
+</script> -->
+
 <script setup>
-    const route = useRouter();
-    // console.log(route.params.id);
-    const nuxt = useNuxtApp();
-    // console.log(nuxt);
+const route = useRoute();
+const id = ref(null);
+
+onMounted(() => {
+    id.value = route.params.id;
+});
 </script>
 
 <template>
-    <div class="mx-auto">
-        <h1 class="p-2 text-4xl font-semibold h-fit">
-            Wildcard: {{ $route.params.id }}</h1>
+    <div class="mx-auto w-full rounded-lg h-full flex flex-col gap-2">
+        <h1 class="mt-2 p-2 text-4xl font-semibold h-fit text-center">
+            Wildcard: {{ id }}
+        </h1>
     </div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
